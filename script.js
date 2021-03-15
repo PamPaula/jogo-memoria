84939cd7a0bb04a7f794cce69b3454e5f16c24dd
PamPaula <pamela.dpaulas@gmail.com>
2021-03-15 11:42:39 -0300

    verificando se as cartas são iguais

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 //virando a carta
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+//vendo se o atributo é igual
+function checkForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
+
