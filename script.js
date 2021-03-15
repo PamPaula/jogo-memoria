72fc8de8328c62681f874eaae14b7a1076771dbd
PamPaula <pamela.dpaulas@gmail.com>
2021-03-15 12:11:21 -0300

    embaralhando as cartas

--- script.js
+++ script.js
@@ -56,6 +56,14 @@ function resetBoard() {
     [firstCard, secondCard] = [null, null];
 }
 
+//embaralhando as cartas
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
+
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
